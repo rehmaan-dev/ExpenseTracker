74e8b8c3a8762ca74da5d50309642adc3e28b606
Amisha Saxena <Amisha.Saxena@motherson.com>
2024-08-02 14:44:12 +0530

    initial commit from amisha

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Range(1,int.MaxValue,ErrorMessage ="Please select a category.")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                var cultureInfo = new CultureInfo("en-IN"); // Replace "en-US" with the desired culture
+                string currencySymbol = cultureInfo.NumberFormat.CurrencySymbol;
+
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0", cultureInfo);
+            }
+        }
+
+    }
+}
